40802e99242735339ec6bae58f78e73f2b5e33cd
jhuang239 <jhuang239-c@my.cityu.edu.hk>
2024-03-29 20:24:29 +0800

    updat

--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,5 +1,5 @@
 def individual_serial(history) -> dict:
-    return{
+    return {
         "id": str(history["_id"]),
         "user_id": history["user_id"],
         "chat_name": history["chat_name"],
@@ -7,11 +7,13 @@ def individual_serial(history) -> dict:
         # "message": history["message"],
     }
 
+
 def list_serial(histories) -> list:
     return [individual_serial(history) for history in histories]
 
+
 def individual_serial_message(history) -> dict:
-    return{
+    return {
         "id": str(history["_id"]),
         "user_id": history["user_id"],
         "chat_name": history["chat_name"],
@@ -19,12 +21,13 @@ def individual_serial_message(history) -> dict:
         "message": history["message"],
     }
 
+
 def list_serial_message(histories) -> list:
     return [individual_serial_message(history) for history in histories]
 
 
 def get_user(user) -> dict:
-    return{
+    return {
         "user_id": user["user_id"],
         "name": user["name"],
         "email": user["email"],
@@ -33,7 +36,6 @@ def get_user(user) -> dict:
         "birthday": user["birthday"],
     }
 
+
 def get_all_user(users) -> list:
     return [get_user(user) for user in users]
-
-
