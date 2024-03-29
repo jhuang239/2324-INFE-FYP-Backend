682b8d845641633e80337c2710c90e651eab6e16
KimWong1118 <jk.wong1118@gmail.com>
2024-03-30 01:15:00 +0800

    update

--- routes/auth.py
+++ routes/auth.py
@@ -72,5 +72,5 @@ async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm,
     user = authenticate_user(form_data.username, form_data.password)
     if(not user):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid authentication credentials")
-    token = create_access_token(user["name"], user["user_id"], user['email'], timedelta(minutes=30))
+    token = create_access_token(user["name"], user["user_id"], user['email'], timedelta(days=30))
     return {'access_token': token, 'token_type': 'bearer'}
