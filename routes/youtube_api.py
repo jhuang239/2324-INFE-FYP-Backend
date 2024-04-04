820532cf614feafbb23178a6cf57ce0b2a510278
KimWong1118 <jk.wong1118@gmail.com>
2024-04-04 15:27:10 +0800

    update

--- routes/youtube_api.py
+++ routes/youtube_api.py
@@ -83,6 +83,7 @@ async def get_video(user: user_dependency, url: str):
     yt = YouTube(url)
     obj = {
         "id": yt.video_id,
+        "url": yt.watch_url,
         "title": yt.title,
         "thumbnail": yt.thumbnail_url,
         "author": yt.author,
