2606d4c4621b4cb71e2a7428920e78e6b63886ff
Ubuntu <ubuntu@ip-172-31-22-148.ap-southeast-2.compute.internal>
2024-04-07 04:38:35 +0000

    Revert "update"
    This reverts commit b6f354106fadbef0b71f547a6f7fc3e2bab0039c.

--- routes/discussion.py
+++ routes/discussion.py
@@ -87,11 +87,8 @@ async def get_discussion(id: str, user: user_dependency):
     discussion = collection_discussion.find_one(
         {"_id": ObjectId(id)}, {"_id": 0})
 
-    if(discussion["banner_img"] != ""):
-        discussion["banner_img"] = get_file_link(discussion["banner_img"])
-    
-    if(len(discussion["files"]) > 0):
-        discussion["files"] = [get_file_link(file) for file in discussion["files"]]
+    discussion["banner_img"] = get_file_link(discussion["banner_img"])
+    discussion["files"] = [get_file_link(file) for file in discussion["files"]]
 
     comments = list(collection_discussion_comment.find(
         {"discussion_id": id}, {"_id": 0}).sort("created_at", ASCENDING))
